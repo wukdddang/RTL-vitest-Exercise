08bdf1a7b777232e0a5db42c3a016b0e2c7ae097
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-12 11:49:46 +0900

    test: /toppings API MSW로 모킹 - findAllByRole로 img를 가지는 컴포넌트 테스트

--- src/pages/entry/ToppingOption.tsx
+++ src/pages/entry/ToppingOption.tsx
@@ -1,15 +1,15 @@
 import Col from 'react-bootstrap/Col'
 
-const ScoopOption = ({ name, imagePath }: { name: string; imagePath: string }) => {
+const ToppingOption = ({ name, imagePath }: { name: string; imagePath: string }) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
         style={{ width: '75%' }}
         src={`http://localhost:3030/${imagePath}`}
-        alt={`${name} scoop`}
+        alt={`${name} topping`}
       />
     </Col>
   )
 }
 
-export default ScoopOption
+export default ToppingOption
--- src/pages/entry/test/Options.test.tsx
+++ src/pages/entry/test/Options.test.tsx
@@ -6,9 +6,21 @@ import Options from '../Options'
 test('서버로부터 받아온 scoop 옵션들이 화면에 보여야 한다.', async () => {
   render(<Options optionType={'scoops'} />)
 
-  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i })
+  const scoopImages = (await screen.findAllByRole('img', { name: /scoop$/i })) as HTMLImageElement[]
   expect(scoopImages).toHaveLength(2)
 
   const altText = scoopImages.map((element) => element.alt)
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
+})
+
+test('서버로부터 받아온 topping 옵션들이 화면에 보여야 한다.', async () => {
+  render(<Options optionType={'toppings'} />)
+
+  const toppingImages = (await screen.findAllByRole('img', {
+    name: /topping$/i,
+  })) as HTMLImageElement[]
+  expect(toppingImages).toHaveLength(3)
+
+  const altText = toppingImages.map((element) => element.alt)
+  expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping'])
 })
