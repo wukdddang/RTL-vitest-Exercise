947a89f0f5e62a0c378bdde111796d9ba53b38ea
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-12 17:03:41 +0900

    chore: tsc 설정 제거 + 브라우저에서도 MSW 사용하도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
-import './App.css'
+import { Container } from 'react-bootstrap'
 
-import { useState } from 'react'
-
-import SummaryForm from './pages/summary/SummaryForm'
-
-export function replaceCamelWithSpaces(colorName: string) {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1')
-}
+import { OrderDetailsProvider } from './contexts/OrderDetails'
+import OrderEntry from './pages/entry/OrderEntry'
 
 const App = () => {
-  const [buttonColor, setButtonColor] = useState('#C71585')
-  const [checkboxChecked, setCheckboxChecked] = useState(false)
-  const newButtonColor = buttonColor === '#C71585' ? '#191970' : '#C71585'
-
   return (
-    <div>
-      <button
-        style={{
-          backgroundColor: checkboxChecked ? 'gray' : buttonColor,
-        }}
-        disabled={checkboxChecked}
-        onClick={() => setButtonColor(newButtonColor)}
-      >
-        {newButtonColor === '#C71585' ? '빨간색' : '파란색'}
-        {'으로 변경'}
-      </button>
-      <input
-        type={'checkbox'}
-        id={'disable-button-checkbox'}
-        defaultChecked={false}
-        onClick={() => setCheckboxChecked(!checkboxChecked)}
-      />
-      <label htmlFor={'disable-button-checkbox'}>{'Disable button'}</label>
-      <SummaryForm />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry />
+      </OrderDetailsProvider>
+    </Container>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  resolve: {
-    conditions: ['browser', 'module', 'import'],
-  },
   plugins: [react()],
 })
